d5d0c294b59e6f320569a836bb763b6350038d4f
Bhalchandra-patil03 <bp154126@gmail.com>
2025-11-15 14:58:56 +0530

    Creating Boilerplate

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// console.log("Bhalchandra patil");
+// name = "Bhalchandra patil";
+// console.log(name);
+// a = 65;
+// b = 10 ;
+// console.log(a*b);
+
+
+var a = [10,20,30,40];
+
+function myfunc(b, ...a) {
+    console.log(a);
+}
+
+myfunc(8,9,10,11,12,13);  // Output: [9, 10, 11, 12, 13]
